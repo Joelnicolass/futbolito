9c60dd3ad657a23005d2e010390da92ad468d44f
Albano <albanokainer@hotmail.com>
2023-11-25 14:42:19 -0300

    Add onboarding data control on utils

---
+++ src/features/core/utils/onboarding_data.ts
@@ -0,0 +1,39 @@
+export const onboardingData = [
+    {
+      id: 1,
+      img: require('../assets/timesense_logo.png'),
+      title: 'Apple Watch Series 7',
+      description: 'The future of health is on your wrist',
+      price: '$399',
+    },
+    {
+      id: 2,
+      img: require('../assets/timesense_logo.png'),
+      title: 'AirPods Pro',
+      description: 'Active noise cancellation for immersive sound',
+      price: '$249',
+    },
+    {
+      id: 3,
+      img: require('../assets/timesense_logo.png'),
+      title: 'AirPods Max',
+      description: 'Effortless AirPods experience',
+      price: '$549',
+    },
+    {
+      id: 4,
+      img: require('../assets/timesense_logo.png'),
+      title: 'Charger',
+      description: "It's not magic, it's just science",
+      price: '$49',
+    },
+    {
+      id: 5,
+      img: require('../assets/timesense_logo.png'),
+      title: 'Smart Lock',
+      description: 'Unlock your door with your phone',
+      price: '$199',
+    },
+  ];
+
+  export const LAST_ITEM = onboardingData.length - 1;
