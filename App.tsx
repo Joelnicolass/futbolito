66996ba1ea16acde281072a42b5958bc1b9e6f15
Albano <albanokainer@hotmail.com>
2023-11-25 14:46:06 -0300

    Add redux provider and change routes names

--- App.tsx
+++ App.tsx
@@ -6,58 +6,62 @@
  * @format
  */
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { FirebasePrueba } from './src/features/presentation/screens/firebase_demo';
-import { HomeScreen } from './src/features/presentation/screens/home/home_screen';
-import { LoadingScreen } from './src/features/presentation/screens/loading/loading_screen';
-import { LoginScreen } from './src/features/presentation/screens/login/login_screen';
-import { RegisterScreen } from './src/features/presentation/screens/register/register_screen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {FirebasePrueba} from './src/features/presentation/screens/firebase_demo';
+import {HomeScreen} from './src/features/presentation/screens/home/home_screen';
+import {LoadingScreen} from './src/features/presentation/screens/loading/loading_screen';
+import {LoginScreen} from './src/features/presentation/screens/login/login_screen';
+import {RegisterScreen} from './src/features/presentation/screens/register/register_screen';
+import {OnboardingScreen} from './src/features/presentation/screens/onboarding/onboarding_screen';
+import {Provider} from 'react-redux';
+import {store} from './src/features/presentation/store/store';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Loading"
-          options={{
-            headerShown: false,
-          }}
-          component={LoadingScreen}
-        />
-        <Stack.Screen
-          name="Register"
-          options={{
-            headerShown: false,
-          }}
-          component={RegisterScreen}
-        />
-        <Stack.Screen
-          name="Login"
-          options={{
-            headerShown: false,
-          }}
-          component={LoginScreen}
-        />
-        <Stack.Screen
-          name="Home"
-          options={{
-            headerShown: false,
-          }}
-          component={HomeScreen}
-        />
-
-        <Stack.Screen
-          name="Auth"
-          options={{
-            headerShown: false,
-          }}
-          component={FirebasePrueba}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="loading"
+            options={{
+              headerShown: false,
+            }}
+            component={LoadingScreen}
+          />
+          <Stack.Screen
+            name="onboarding"
+            options={{
+              headerShown: false,
+            }}
+            component={OnboardingScreen}
+          />
+          <Stack.Screen
+            name="register"
+            options={{
+              headerShown: false,
+            }}
+            component={RegisterScreen}
+          />
+          <Stack.Screen
+            name="login"
+            options={{
+              headerShown: false,
+            }}
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            name="home"
+            options={{
+              headerShown: false,
+            }}
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
