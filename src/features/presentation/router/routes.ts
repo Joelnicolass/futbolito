3554671252c2daabfbd4394c809f20e8e318f89d
joelsartori <joelnicolass@gmail.com>
2023-11-25 22:05:37 -0300

    [fix] - ios cfg

--- src/features/presentation/router/routes.ts
+++ src/features/presentation/router/routes.ts
@@ -1,9 +1,8 @@
 export const ROUTES = {
-    REGISTER: 'register',
-    LOGIN: 'login',
-    HOME: 'home',
-    CREATE_MATCH: 'create-match',
-    PROFILE: 'profile',
-    ONBOARDING: 'onboarding',
-
+  REGISTER: 'register',
+  LOGIN: 'login',
+  HOME: 'home',
+  CREATE_MATCH: 'create-match',
+  PROFILE: 'profile',
+  ONBOARDING: 'onboarding',
 };
