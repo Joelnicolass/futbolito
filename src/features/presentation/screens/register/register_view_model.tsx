1c8ebd1d98beb6c9e18453ad8c47afc309880d37
Albano <albanokainer@hotmail.com>
2023-11-26 08:08:17 -0300

    Update formik valitation schema

--- src/features/presentation/screens/register/register_view_model.tsx
+++ src/features/presentation/screens/register/register_view_model.tsx
@@ -1,12 +1,11 @@
 //import liraries
 import {StackActions, useNavigation} from '@react-navigation/native';
 import * as yup from 'yup';
-import { SignUpUseCase } from '../../../domain/usecases/signup/signup_use_case';
+import {SignUpUseCase} from '../../../domain/usecases/signup/signup_use_case';
+import {ROUTES} from '../../router/routes';
 
 const signupValidationSchema = yup.object().shape({
-  name: yup
-    .string()
-    .required('Name is Required'),
+  name: yup.string().required('Name is Required'),
   email: yup
     .string()
     .email('Please enter valid email')
@@ -18,14 +17,14 @@ const signupValidationSchema = yup.object().shape({
     .required('Password is required'),
   repassword: yup
     .string()
-    .min(8, ({min}) => `Password must be at least ${min} characters`)
-    .required('Password is required'),
+    .required('Password is required')
+    .oneOf([yup.ref('password')], 'Passwords must match'),
 });
 
 const initialFormState = {name: '', email: '', repassword: '', password: ''};
 // create a component
 export const RegisterViewModel = () => {
-  const signUpUseCase = new SignUpUseCase()
+  const signUpUseCase = new SignUpUseCase();
   const navigation = useNavigation();
   const onPressLogin = () => {
     // Do something about login operation
@@ -38,13 +37,17 @@ export const RegisterViewModel = () => {
   const validateForm = async (userFormValues: FormState, {setErrors}: any) => {
     try {
       const {name, email, password} = userFormValues;
-     
-      const userCredential =  await signUpUseCase.register(name,email,password)
-      
+
+      const userCredential = await signUpUseCase.register(
+        name,
+        email,
+        password,
+      );
+
       console.log(userCredential);
-      
+
       navigation.dispatch(
-        StackActions.replace('Home', {state: userCredential.user}),
+        StackActions.replace(ROUTES.MAIN, {state: userCredential.user}),
       );
     } catch (error: any) {
       if (error.code === 'auth/email-already-exists') {
@@ -52,6 +55,8 @@ export const RegisterViewModel = () => {
       }
     }
   };
+
+
   return {
     validateForm,
     onPressForgotPassword,
