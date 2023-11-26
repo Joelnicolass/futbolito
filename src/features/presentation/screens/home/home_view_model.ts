a2f619eb178a9db775bd8bfafcebaa1769414daa
Albano <albanokainer@hotmail.com>
2023-11-26 08:04:20 -0300

    Add new handleLogout function

--- src/features/presentation/screens/home/home_view_model.ts
+++ src/features/presentation/screens/home/home_view_model.ts
@@ -1,9 +1,18 @@
 import {StackActions, useNavigation} from '@react-navigation/native';
+import { LogoutUseCase } from '../../../domain/usecases/auth/logout_use_case';
+import { useRouter } from '../../hooks/useRouter';
+import { ROUTES } from '../../router/routes';
 
 export const HomeViewModel = () => {
   const navigation = useNavigation();
+  const logoutUseCase = new LogoutUseCase();
+  const {handleReplace} = useRouter();
   const handleTap = () => {
     navigation.dispatch(StackActions.replace('Login'));
+  };
+  const handleLogout = () => {
+    logoutUseCase.logout();
+    handleReplace(ROUTES.LOGIN);
   };
   const handleAuth = () => {
     navigation.dispatch(StackActions.replace('Auth'));
@@ -11,5 +20,6 @@ export const HomeViewModel = () => {
   return {
     handleTap,
     handleAuth,
+    handleLogout,
   };
 };
