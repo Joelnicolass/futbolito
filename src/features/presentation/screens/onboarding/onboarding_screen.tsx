02a12c44ddfd1bbaf866699e2dfab42ff0f478cf
Albano <albanokainer@hotmail.com>
2023-11-26 08:10:15 -0300

    Minor

--- src/features/presentation/screens/onboarding/onboarding_screen.tsx
+++ src/features/presentation/screens/onboarding/onboarding_screen.tsx
@@ -1,12 +1,7 @@
 import {default as React} from 'react';
 import {
-  ActivityIndicator,
-  Button,
   SafeAreaView,
   Text,
-  TextInput,
-  TouchableOpacity,
-  View,
 } from 'react-native';
 import {OnboardingViewModel} from './onboarding_view_model';
 import {styles} from './onboarding_styles';
@@ -14,11 +9,11 @@ import Slider from '../../components/app_onboarding/slider/slider';
 import {onboardingData} from '../../../core/utils/onboarding_data';
 
 export const OnboardingScreen = () => {
-  const {getItem} = OnboardingViewModel();
+  const {} = OnboardingViewModel();
   return (
     <SafeAreaView>
       <Text style={styles.bottomText}>Hola</Text>
-      <Slider data={onboardingData} currentItem={getItem} />
+      <Slider data={onboardingData} />
     </SafeAreaView>
   );
 };
