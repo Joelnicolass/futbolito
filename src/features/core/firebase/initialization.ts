75b25b0a89d35d4d4ca959a4b027b27fecc34ecf
Albano <albanokainer@hotmail.com>
2023-12-08 19:06:30 -0300

    Initialize firestore

--- src/features/core/firebase/initialization.ts
+++ src/features/core/firebase/initialization.ts
@@ -1,8 +1,7 @@
 import {initializeApp} from '@firebase/app';
 import {getAuth, setPersistence} from '@firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
   apiKey: 'AIzaSyBxWCMBXErjN1jb07gYRx66YLxuDtFmSmQ',
   authDomain: 'futbolito-a8dea.firebaseapp.com',
@@ -16,6 +15,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 setPersistence(auth, {type: 'LOCAL'});
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+
+// Initialize Firebase
+
+
+// Initialize Cloud Firestore and get a reference to the service
 
 // export const analytics = getAnalytics(app);
