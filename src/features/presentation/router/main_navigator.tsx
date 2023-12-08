931f643cd0d1f1ed1f3ec02a17057ab70e6108d0
Albano <albanokainer@hotmail.com>
2023-12-08 19:15:14 -0300

    Comment all screens and only set home for developing

--- src/features/presentation/router/main_navigator.tsx
+++ src/features/presentation/router/main_navigator.tsx
@@ -1,59 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {OnboardingScreen} from '../screens/onboarding/onboarding_screen';
 import {LoadingScreen} from '../screens/loading/loading_screen';
 import {RegisterScreen} from '../screens/register/register_screen';
 import {LoginScreen} from '../screens/login/login_screen';
 import {createStackNavigator} from '@react-navigation/stack';
-import { TabNavigator } from './tab_navigator';
-import { IsAuthenticatedUseCase } from '../../domain/usecases/auth/is_authenticated_use_case';
+import {TabNavigator} from './tab_navigator';
+import {IsAuthenticatedUseCase} from '../../domain/usecases/auth/is_authenticated_use_case';
+import { useAuth } from '../hooks/useAuth';
 const Stack = createStackNavigator();
 
 export const MainNavigator = () => {
-  const user = new IsAuthenticatedUseCase();
-return(
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="home">
-      <Stack.Screen
-        name="loading"
-        options={{
-          headerShown: false,
-          tabBarStyle: {display: 'none'},
-        }}
-        component={LoadingScreen}
-      />
-      <Stack.Screen
-        name="onboarding"
-        options={{
-          headerShown: false,
-          tabBarStyle: {display: 'none'},
-        }}
-        component={OnboardingScreen}
-      />
-      <Stack.Screen
-        name="register"
-        options={{
-          headerShown: false,
-          tabBarStyle: {display: 'none'},
-        }}
-        component={RegisterScreen}
-      />
-      <Stack.Screen
-        name="login"
-        options={{
-          headerShown: false,
-          tabBarStyle: {display: 'none'},
-        }}
-        component={LoginScreen}
-      />
-      {user.isAuthenticated() && <Stack.Screen
-        name="home"
-        options={{
-          headerShown: false,
-        }}
-        component={TabNavigator}
-      />}
-    </Stack.Navigator>
-  </NavigationContainer>
-);
-}
+  const {isAuth} = useAuth();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen
+          name="loading"
+          options={{
+            headerShown: false,
+            tabBarStyle: {display: 'none'},
+          }}
+          component={LoadingScreen}
+        />
+        <Stack.Screen
+          name="onboarding"
+          options={{
+            headerShown: false,
+            tabBarStyle: {display: 'none'},
+          }}
+          component={OnboardingScreen}
+        />
+        <Stack.Screen
+          name="register"
+          options={{
+            headerShown: false,
+            tabBarStyle: {display: 'none'},
+          }}
+          component={RegisterScreen}
+        />
+        <Stack.Screen
+          name="login"
+          options={{
+            headerShown: false,
+            tabBarStyle: {display: 'none'},
+          }}
+          component={LoginScreen}
+        /> */}
+          <Stack.Screen
+            name="home"
+            options={{
+              headerShown: false,
+            }}
+            component={TabNavigator}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
