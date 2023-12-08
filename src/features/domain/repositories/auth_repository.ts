bd990a47616894b83ca9b429b72348538313dac4
Albano <albanokainer@hotmail.com>
2023-12-08 18:57:36 -0300

    refactor auth repository

--- src/features/domain/repositories/auth_repository.ts
+++ src/features/domain/repositories/auth_repository.ts
@@ -3,5 +3,5 @@ import { UserCredential } from "@firebase/auth";
 export abstract class IAuthRepository {
   abstract login(email: string, password: string): Promise<UserCredential>;
   abstract logout(): Promise<void>;
-  abstract isAuthenticated(): boolean;
+  abstract isAuthenticated(): Promise<boolean>;
 }
