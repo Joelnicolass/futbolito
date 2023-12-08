6c2648dbf1d628391e721b7ebfed191f0c477785
Albano <albanokainer@hotmail.com>
2023-12-08 18:57:00 -0300

    move firebase auth datasource to domain

--- src/features/domain/datasource/auth_firebase_datasource.ts
+++ src/features/domain/datasource/auth_firebase_datasource.ts
@@ -1,10 +1,10 @@
-import {User} from '../../../domain/entities/user';
 import {UserCredential} from '@firebase/auth';
+import { User } from '../entities/user';
 
 export abstract class IAuthFirebaseDataSource {
   abstract register(name:string,email: string, password: string): Promise<UserCredential>;
   abstract login(email: string, password: string): Promise<UserCredential>;
-  abstract isAuthenticated(): boolean;
+  abstract isAuthenticated(): Promise<boolean>;
   abstract getUser(): Promise<User>;
   abstract logout(): Promise<void>;
 }
