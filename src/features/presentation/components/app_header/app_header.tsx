3299d3b25a18a98c2634f7a851ca1310e22ac315
joelsartori <joelnicolass@gmail.com>
2023-12-08 23:08:53 -0300

    add show avatar option in header

--- src/features/presentation/components/app_header/app_header.tsx
+++ src/features/presentation/components/app_header/app_header.tsx
@@ -10,22 +10,27 @@ import {gradient} from '../../theme/theme';
 
 const AppHeader = () => {
   const insets = useSafeAreaInsets();
-  const {title, subtitle} = useAppSelector(state => state.header);
+  const {title, subtitle, showAvatar} = useAppSelector(state => state.header);
   // TODO usar estado de user para el avatar y el nombre
 
   return (
     <LinearGradient colors={gradient} style={styles.container}>
-      <View style={[styles.sectionTitle, {marginTop: insets.top}]}>
-        <AppAvatar
-          align="left"
-          text="joelnicolass@gmail.com"
-          size={80}
-          row
-          bold
-          showRating
-        />
-      </View>
-      <AppDivider />
+      {showAvatar && (
+        <>
+          <View style={[styles.sectionTitle, {marginTop: insets.top}]}>
+            <AppAvatar
+              align="left"
+              text="joelnicolass@gmail.com"
+              size={80}
+              row
+              bold
+              showRating
+            />
+          </View>
+          <AppDivider />
+        </>
+      )}
+      {!showAvatar && <View style={[{marginTop: insets.top}]} />}
       <AppText bold>{title}</AppText>
       <AppText size="sm">{subtitle}</AppText>
     </LinearGradient>
