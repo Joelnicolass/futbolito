7f8934a3faf0a28bd05225ec6444220b5a204d9a
Albano <albanokainer@hotmail.com>
2024-01-18 00:21:56 -0300

    MINOR: remove some imports

--- src/features/presentation/screens/match/match_view_model.ts
+++ src/features/presentation/screens/match/match_view_model.ts
@@ -2,9 +2,6 @@ import { useEffect, useState } from 'react';
 import {matchUseCases} from '../../../data/usecases/match_use_cases_impl';
 import {Alert} from 'react-native';
 import {useHeader} from '../../hooks/use_header';
-import { GetMatchesUseCase } from '../../../domain/usecases/match/get_matches_use_case';
-import { MatchRepositoryImpl } from '../../../data/repositories/match_repository_impl';
-import { MatchDatasourceImpl } from '../../../data/datasources/remote/match_datasource_impl';
 import { Match } from '../../../domain/entities/match';
 
 export const MatchViewModel = () => {
